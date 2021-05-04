8431963a414afdbb27e479d02ff58677da6385df
enzo <enzo.munooz@gmail.com>
2021-05-04 20:19:10 -0300

    Se agrega excepcion

---
+++ services/src/main/java/com/ideaas/services/service/ColaboradorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ideaas.services.service;
+
+public class ColaboradorNotFoundException extends Exception {
+    public ColaboradorNotFoundException(String message) {
+        super(message);
+    }
+}
